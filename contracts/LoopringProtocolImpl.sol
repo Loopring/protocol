12580c7a215c9327ee04eb6d82c135b11d2d7d0f
akash-bansal <akash.bansal@flipkart.com>
2017-11-04 01:30:27 +0530

    Instance creation

--- contracts/LoopringProtocolImpl.sol
+++ contracts/LoopringProtocolImpl.sol
@@ -603,7 +603,8 @@ contract LoopringProtocolImpl is LoopringProtocol {
         internal
         constant
     {
-        uint minerLrcSpendable = getLRCSpendable(ring.feeRecepient);
+        TokenTransferDelegate tokenTransferDelegate = TokenTransferDelegate(delegateAddress);
+        uint minerLrcSpendable = tokenTransferDelegate.getSpendable(lrcTokenAddress, ring.feeRecepient);
         uint ringSize = ring.orders.length;
 
         for (uint i = 0; i < ringSize; i++) {
@@ -612,7 +613,7 @@ contract LoopringProtocolImpl is LoopringProtocol {
 
             if (state.feeSelection == FEE_SELECT_LRC) {
 
-                uint lrcSpendable = getLRCSpendable(state.order.owner);
+                uint lrcSpendable = tokenTransferDelegate.getSpendable(lrcTokenAddress, state.order.owner);
 
                 if (lrcSpendable < state.lrcFee) {
                     if (ring.throwIfLRCIsInsuffcient) {
