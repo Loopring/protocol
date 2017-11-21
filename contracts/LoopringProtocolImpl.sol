05b968104143591e006a0d2c1ecafd31407511ad
Hephyrius <Hephyrius@gmail.com>
2017-11-21 17:04:48 +0800

    Small Logic Updates to Calculations

--- contracts/LoopringProtocolImpl.sol
+++ contracts/LoopringProtocolImpl.sol
@@ -558,7 +558,6 @@ contract LoopringProtocolImpl is LoopringProtocol {
     {
         bool checkedMinerLrcSpendable = false;
         uint minerLrcSpendable = 0;
-        uint8 _marginSplitPercentageBase = MARGIN_SPLIT_PERCENTAGE_BASE;
 
         for (uint i = 0; i < ringSize; i++) {
             var state = orders[i];
@@ -569,7 +568,12 @@ contract LoopringProtocolImpl is LoopringProtocol {
                 // When an order's LRC fee is 0 or smaller than the specified fee,
                 // we help miner automatically select margin-split.
                 state.feeSelection = FEE_SELECT_MARGIN_SPLIT;
-                state.order.marginSplitPercentage = _marginSplitPercentageBase;
+                if (marginSplitAndNoMoreB >= 128) {
+                    state.order.marginSplitAndNoMoreB = MARGIN_SPLIT_PERCENTAGE_BASE+128;
+                } else {
+                    state.order.marginSplitAndNoMoreB = MARGIN_SPLIT_PERCENTAGE_BASE;
+                }
+                
             } else {
                 uint lrcSpendable = getSpendable(
                     delegate,
@@ -593,7 +597,12 @@ contract LoopringProtocolImpl is LoopringProtocol {
                 // If order doesn't have enough LRC, set margin split to 100%.
                 if (lrcTotal < state.lrcFee) {
                     state.lrcFee = lrcTotal;
-                    state.order.marginSplitPercentage = _marginSplitPercentageBase;
+
+                    if (marginSplitAndNoMoreB >= 128) {
+                        state.order.marginSplitAndNoMoreB = MARGIN_SPLIT_PERCENTAGE_BASE+128;
+                    } else {
+                        state.order.marginSplitAndNoMoreB = MARGIN_SPLIT_PERCENTAGE_BASE;
+                        }
                 }
 
                 if (state.lrcFee == 0) {
@@ -644,10 +653,10 @@ contract LoopringProtocolImpl is LoopringProtocol {
                         pct = state.order.marginSplitAndNoMoreB;
                     }
 
-                    if (pct != _marginSplitPercentageBase) {
+                    if (pct != MARGIN_SPLIT_PERCENTAGE_BASE) {
                         split = split.mul(
                             pct
-                        ) / _marginSplitPercentageBase;
+                        ) / MARGIN_SPLIT_PERCENTAGE_BASE;
                     }
 
                     if (state.order.marginSplitAndNoMoreB >= 128) {
